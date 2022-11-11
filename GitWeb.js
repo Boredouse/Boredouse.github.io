94f5b3d60a910dd54ec911f8c80fdda4beca5b7c
Boredouse <106819055+Boredouse@users.noreply.github.com>
2022-11-11 12:41:30 +0000

    Add files via upload

--- GitWeb.js
+++ GitWeb.js
@@ -1,15 +1,7 @@
-const OpenMenu = document.querySelector('')
-const HideMenu = document.querySelector('')
-const SideMenu = document.querySelector('')
 
-OpenMenu.addEventListener('click', function() {
-  SideMenu.classlist.add('active')
-}
-)
 
-function blink_text() {
-	$('.blink').fadeOut(500);
-    $('.blink').fadeIn(500);
-}
-setInterval(blink_text, 1000);
+
+
+
+
 
